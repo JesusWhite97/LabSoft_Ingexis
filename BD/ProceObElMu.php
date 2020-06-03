1cfeb92cdf1f0840e4f9ded5eff3b84632c7c36d
JesusWhite97 <jesus120190240.8@gmail.com>
2020-06-02 20:46:04 -0600

    master

---
+++ BD/ProceObElMu.php
@@ -0,0 +1,29 @@
+<?php
+    // ========================================================
+    include '../Logica/CrearDirectorios.php';
+    include 'conexion.php';
+    header("Content-Type: text/html;charset=utf-8"); 
+    // ========================================================
+    class procedimientos_Obra{
+        //#####################################################
+        public function obraAgregar($id_client, $nombre, $direccion, $anotaciones){
+            //crea Conexion===============
+            $conex = new conexionMySQLi();
+            $mysqli = $conex->conexion($_SESSION['puesto']);
+            //============================
+            mysqli_query($mysqli, "SET NAMES 'utf8'");
+            $query = "CALL Obra_agregar('".$id_client."', '".$nombre."', '".$direccion."', '".$anotaciones."')";
+            if($mysqli->query($query)===TRUE){
+                return 'true';
+            }else{
+                return "Error: ".$mysqli->error;
+            } 
+        }
+        //#####################################################
+        public function obraEliminar(){
+            
+        }
+        //#####################################################
+    }
+    // ========================================================
+?>
